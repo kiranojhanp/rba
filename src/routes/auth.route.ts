1c5101a7813aadddda3e753398e6e627ece76916
Kiran Ojha <54209728+kiranojhanp@users.noreply.github.com>
2022-06-08 15:10:21 +0545

    rename middleeares

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,11 @@
 import { Router } from "express"
 import { admin, login, register } from "../controllers/auth.controller"
-import { verifyAuthentication, verifyAuthorization } from "../middlewares/auth.middleware"
+import { verifyAuthentication as authenticate, verifyAuthorization as allow } from "../middlewares/auth.middleware"
 
 const router = Router()
 
 router.post("/register", register)
 router.post("/login", login)
-router.post("/admin", verifyAuthentication, verifyAuthorization(["admin", "super-admin"]), admin)
+router.post("/admin", authenticate, allow(["admin", "super-admin"]), admin)
 
 export default router
