b99f3c71edd9fadd6f8caa32269c3b3d761aa688
Kiran Ojha <54209728+kiranojhanp@users.noreply.github.com>
2022-06-08 12:32:10 +0545

    authorization middlewares

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from "express"
+import createError from "http-errors"
+import JWT from "jsonwebtoken"
+import { roles } from "../types/roles"
+const { ACCESS_TOKEN_SECRET } = process.env as { [key: string]: string }
+
+const verifyAuthentication = (req: Request, res: Response, next: NextFunction) => {
+    if (!req.headers["authorization"]) return next(new createError.Unauthorized())
+    const authHeader = req.headers["authorization"]
+    const bearerToken = authHeader.split(" ")
+    const token = bearerToken[1]
+    JWT.verify(token, ACCESS_TOKEN_SECRET, (err, payload) => {
+        if (err) {
+            const message = err.name === "JsonWebTokenError" ? "Unauthorized" : err.message
+            return next(new createError.Unauthorized(message))
+        }
+        req.payload = payload
+        next()
+    })
+}
+
+const verifyAuthorization = (allowedRoles: roles[]) => (req: Request, res: Response, next: NextFunction) => {
+    let allowed = false
+    allowedRoles.forEach((role) => {
+        if (req?.payload?.role === role) {
+            allowed = true
+        }
+    })
+
+    if (!allowed) return new createError.Unauthorized("You don't have enough priviledge to view this resource.")
+    next()
+}
+
+export { verifyAuthentication, verifyAuthorization }
---
+++ src/types/roles.ts
@@ -0,0 +1,1 @@
+export type roles = "basic" | "admin" | "super-admin"
