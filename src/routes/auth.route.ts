fd90527423e5f189a1b5ce07dbde5389a314d95b
Kiran Ojha <54209728+kiranojhanp@users.noreply.github.com>
2022-06-08 14:53:30 +0545

    authorization verified

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -6,6 +6,6 @@ const router = Router()
 
 router.post("/register", register)
 router.post("/login", login)
-router.post("/admin", verifyAuthentication, verifyAuthorization(["admin"]), admin)
+router.post("/admin", verifyAuthentication, verifyAuthorization(["admin", "super-admin"]), admin)
 
 export default router
